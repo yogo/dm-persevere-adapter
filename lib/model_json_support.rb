c17b5e77437b9b8b85c872044b3f022f8bd6eed3
Robbie Lamb <robbie.lamb@gmail.com>
2010-01-13 11:02:22 -0700

    Updated to_json_schema to account for optional types.

--- lib/model_json_support.rb
+++ lib/model_json_support.rb
@@ -1,45 +1,51 @@
 module DataMapper
   module Model
     # module Json
-      def to_json_schema
-        # usable_properties = properties.select{|p| p.name != :id }
-        # "{ \"id\" : \"#{self.name}\",\n \t\"properties\": {\n" + usable_properties.collect{|p| "\t\t\"#{p.name}\" : { \"type\" : \"#{to_json_type(p.type)}\" }" }.join(",\n ") + "\n\t}\n}"
-        to_json_schema_compatable_hash.to_json
+      def to_json_schema(repository_name = default_repository_name)
+        to_json_schema_compatible_hash(repository_name).to_json
       end
       
       #TODO: Add various options in.
-      def to_json_schema_compatible_hash
+      def to_json_schema_compatible_hash(repository_name = default_repository_name)
         usable_properties = properties.select{|p| p.name != :id }
         schema_hash = {}
-        schema_hash['id'] = self.storage_name
+        schema_hash['id'] = self.storage_name(repository_name)
         properties_hash = {}
-        usable_properties.each{|p| properties_hash[p.name]={ "type" => to_json_type(p.type) } if p.name != :id}
+        usable_properties.each{|p| properties_hash[p.name] = p.to_json_schema_hash if p.name != :id }
         schema_hash['properties'] = properties_hash
-
         return schema_hash
       end
-      
-      private
-      
-      def to_json_type(type)
-        # A case statement doesn't seem to be working when comparing classes.
-        # That's why we're using a if elseif block.
-        if type == DataMapper::Types::Serial
-          return "string"
-        elsif type == String
-          return "string"
-        elsif type == Float
-          return "number"
-        elsif type == DataMapper::Types::Boolean
-          return "boolean"
-        elsif type == DataMapper::Types::Text
-          elsif type == "string"
-        elsif type == Integer
-          return "integer"
-        else 
-          return"string"
-        end
+
+  end
+  
+  class Property
+    def to_json_schema_hash
+      json_hash = { "type" => to_json_type }
+      { "optional" => true }.merge(json_hash) unless required? == true
+      # MIN
+      # MAX
+    end
+    
+    private
+    
+    def to_json_type
+      # A case statement doesn't seem to be working when comparing classes.
+      # That's why we're using a if elseif block.
+      if type == DataMapper::Types::Serial
+        return "string"
+      elsif type == String
+        return "string"
+      elsif type == Float
+        return "number"
+      elsif type == DataMapper::Types::Boolean
+        return "boolean"
+      elsif type == DataMapper::Types::Text
+        elsif type == "string"
+      elsif type == Integer
+        return "integer"
+      else 
+        return"string"
       end
-    # end
+    end
   end
 end
