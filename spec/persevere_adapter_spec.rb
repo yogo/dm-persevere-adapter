b33258f855819cf77e3f6b0dc78e0e668b7063c1
irjudson <irjudson@gmail.com>
2010-01-14 09:31:12 -0700

    updated and cleaned up spec tests.

--- spec/persevere_adapter_spec.rb
+++ spec/persevere_adapter_spec.rb
@@ -10,6 +10,7 @@ describe DataMapper::Adapters::PersevereAdapter do
   before :all do
     # This needs to point to a valid persevere server
     @adapter = DataMapper.setup(:default, { :adapter => 'persevere', :host => 'localhost', :port => '8080' })
+    @repository = DataMapper.repository(@adapter.name)
 
     class Bozon
       include DataMapper::Resource
@@ -55,12 +56,15 @@ describe DataMapper::Adapters::PersevereAdapter do
     }
   end
 
+
+  it_should_behave_like 'An Adapter'
+
   describe 'migrations' do
     it 'should create the Bozon storage' do
       Bozon.auto_migrate!
       Bozon.auto_migrate_down!
     end
-    
+
     it "should destroy Create then Remove the Bozon Storage" do
       @adapter.get_schema(Bozon.storage_name).should == false
       Bozon.auto_migrate_up!
@@ -68,81 +72,83 @@ describe DataMapper::Adapters::PersevereAdapter do
       Bozon.auto_migrate_down!
       @adapter.get_schema(Bozon.storage_name).should == false
     end
+
+    describe '#put_schema' do
+      it 'should create the json schema for the hash' do
+        @adapter.put_schema(@test_schema_hash).should_not == false
+      end 
+
+      it 'should create the json schema for the hash under the specified project' do
+        @adapter.put_schema(@test_schema_hash, "test").should_not == false
+      end
+
+      it 'should create the json schema for the hash under the specified project' do
+        @adapter.put_schema(@test_schema_hash_alt).should_not == false
+      end 
+    end
+
+    describe '#get_schema' do
+      it 'should return all of the schemas (in json) if no name is provided' do
+        result = @adapter.get_schema()
+        result.should_not == false
+        JSON.parse(result).class.should == Array
+      end 
+
+      it 'should return the json schema of the class specified' do
+        result = @adapter.get_schema("Object")
+        result.should_not == false
+        JSON.parse(result)["id"].should == "Object"
+      end
+
+      # I don't think we need these tests.
+      # it 'should return all of the schemas (in json) for a project if no name is provided' do
+      #   result = @adapter.get_schema(nil, "Class")
+      #   debugger
+      #   result
+      # end 
+      #   
+      # it 'should return all of the schemas (in json) if no name is provided' do
+      #   @adapter.get_schema("Object", "Class")
+      # end 
+    end
+
+    describe '#update_schema' do
+      it 'should update a previously existing schema' do
+        result = @adapter.update_schema(@test_schema_hash_mod)
+        result.should_not == false
+
+        @test_schema_hash_mod['id'].should match(JSON.parse(result)['id'])
+      end
+
+      it 'should update a previously created schema under the specified project' do
+        result = @adapter.update_schema(@test_schema_hash_mod, "test")
+        result.should_not == false
+        @test_schema_hash_mod['id'].should match(JSON.parse(result)['id'])
+      end
+
+      it 'should update a previously created schema under the specified project' do
+        result = @adapter.update_schema(@test_schema_hash_alt_mod)
+        result.should_not == false
+        @test_schema_hash_alt_mod['id'].should match(JSON.parse(result)['id'])
+      end
+    end
+
+    describe '#delete_schema' do
+      it 'should delete the specified schema' do
+        @adapter.delete_schema(@test_schema_hash).should == true
+      end
+
+      it 'should delete the specified schema in the specified project' do
+        @adapter.delete_schema(@test_schema_hash, "test").should == true
+      end
+
+      it 'should delete the specified schema in the specified project' do
+        @adapter.delete_schema(@test_schema_hash_alt).should == true
+      end
+    end
   end
 
-  it_should_behave_like 'An Adapter Foo'
-
-  describe '#put_schema' do
-     it 'should create the json schema for the hash' do
-       @adapter.put_schema(@test_schema_hash).should_not == false
-     end 
-  
-     it 'should create the json schema for the hash under the specified project' do
-       @adapter.put_schema(@test_schema_hash, "test").should_not == false
-     end
-  
-     it 'should create the json schema for the hash under the specified project' do
-       @adapter.put_schema(@test_schema_hash_alt).should_not == false
-     end 
-   end
-
-  describe '#get_schema' do
-    it 'should return all of the schemas (in json) if no name is provided' do
-      result = @adapter.get_schema()
-      result.should_not == false
-      JSON.parse(result).class.should == Array
-    end 
-  
-    it 'should return the json schema of the class specified' do
-      result = @adapter.get_schema("Object")
-      result.should_not == false
-      JSON.parse(result)["id"].should == "Object"
-    end
-  
-    # I don't think we need these tests.
-    # it 'should return all of the schemas (in json) for a project if no name is provided' do
-    #   result = @adapter.get_schema(nil, "Class")
-    #   debugger
-    #   result
-    # end 
-    #   
-    # it 'should return all of the schemas (in json) if no name is provided' do
-    #   @adapter.get_schema("Object", "Class")
-    # end 
-  end
-
-  describe '#update_schema' do
-    it 'should update a previously existing schema' do
-      result = @adapter.update_schema(@test_schema_hash_mod)
-      result.should_not == false
-
-      @test_schema_hash_mod['id'].should match(JSON.parse(result)['id'])
-    end
-      
-    it 'should update a previously created schema under the specified project' do
-      result = @adapter.update_schema(@test_schema_hash_mod, "test")
-      result.should_not == false
-      @test_schema_hash_mod['id'].should match(JSON.parse(result)['id'])
-    end
-      
-    it 'should update a previously created schema under the specified project' do
-      result = @adapter.update_schema(@test_schema_hash_alt_mod)
-      result.should_not == false
-      @test_schema_hash_alt_mod['id'].should match(JSON.parse(result)['id'])
-    end
-  end
-
-  describe '#delete_schema' do
-    it 'should delete the specified schema' do
-      @adapter.delete_schema(@test_schema_hash).should == true
-    end
-  
-    it 'should delete the specified schema in the specified project' do
-      @adapter.delete_schema(@test_schema_hash, "test").should == true
-    end
-  
-    it 'should delete the specified schema in the specified project' do
-      @adapter.delete_schema(@test_schema_hash_alt).should == true
-    end
+  describe 'aggregates' do
+    it 'should return the number of items found when .count is called'
   end
 end
