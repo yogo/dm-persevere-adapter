cf4135b7ccd07516cc4256586e61ad67c7338803
Robbie Lamb <robbie.lamb@gmail.com>
2010-06-02 16:30:31 -0600

    Handle more complex types.

--- lib/dm/property.rb
+++ lib/dm/property.rb
@@ -2,15 +2,21 @@ module DataMapper
   class Property
 
     def to_json_schema_hash(repo)
+
       tm = repository(repo).adapter.type_map
+      type_information = tm[type]
+      if type_information.nil? && type.respond_to?(:primitive)
+        type_information = tm[type.primitive]
+      end
+      
       json_hash = Hash.new
-      json_hash = {      "type"      => tm[type][:primitive] }
+      json_hash = {      "type"      => type_information[:primitive] }
       json_hash.merge!({ "optional"  => true })       unless required?
       json_hash.merge!({ "unique"    => true})        if     unique?
       json_hash.merge!({ "position"  => @position })  unless @position.nil?
       json_hash.merge!({ "prefix"    => @prefix })    unless @prefix.nil?
       json_hash.merge!({ "separator" => @separator }) unless @separator.nil?
-      json_hash.merge!(  tm[type].reject{ |key,value| key == :primitive } )
+      json_hash.merge!(  type_information.reject{ |key,value| key == :primitive } )
 
       json_hash
     end
