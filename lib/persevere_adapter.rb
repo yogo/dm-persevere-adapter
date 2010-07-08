83af2ee1367b12eb65679d0d029f92596e35b491
Robbie Lamb <robbie.lamb@gmail.com>
2010-07-08 11:46:09 -0600

    Adding in headers to the debugging

--- lib/persevere_adapter.rb
+++ lib/persevere_adapter.rb
@@ -387,7 +387,7 @@ module DataMapper
         json_query, headers = query.to_json_query
         
         path = "/#{tblname}/#{json_query}"
-        DataMapper.logger.debug("--> PATH/QUERY: #{path}")
+        DataMapper.logger.debug("--> PATH/QUERY/HEADERS: #{path} #{headers.inspect}")
         
         response = @persevere.retrieve(path, headers)
         
