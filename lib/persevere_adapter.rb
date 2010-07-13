ddea9114c89c600c027fd9fb766e14f056aedecc
Ryan Heimbuch <rheimbuch@gmail.com>
2010-07-13 11:28:29 -0600

    Fix top level requires

--- lib/persevere_adapter.rb
+++ lib/persevere_adapter.rb
@@ -16,11 +16,14 @@ require 'persevere_client'
 
 # Require in Adapter modules
 require 'persevere_adapter/query'
+require 'persevere_adapter/associations/many_to_many.rb'
+require 'persevere_adapter/associations/relationship.rb'
+require 'persevere_adapter/resource'
 
 require 'persevere_adapter/support/big_decimal'
 
 require 'persevere_adapter/json_support'
-require 'persevere_adapter/proxy'
+require 'persevere_adapter/enhance'
 
 require 'persevere_adapter/adapter'
 require 'persevere_adapter/migrations'
